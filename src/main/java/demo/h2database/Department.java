4f8138c9dfa3ea2dc10c2661b44029e6ec7c1cf4
Tine Marbjerg <tinemarbjerg@gmail.com>
2024-03-14 13:53:52 +0100

    make test for h2

---
+++ src/main/java/demo/h2database/Department.java
@@ -0,0 +1,26 @@
+package demo.h2database;
+
+public class Department {
+    private int deptno;
+    private String name;
+    private String loc;
+
+    public Department(int deptno, String name, String loc) {
+        this.deptno = deptno;
+        this.name = name;
+        this.loc = loc;
+    }
+
+    public int getDeptno() {
+        return deptno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLoc() {
+        return loc;
+    }
+}
+
