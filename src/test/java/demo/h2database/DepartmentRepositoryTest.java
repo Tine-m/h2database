42c415651373886c5ea3de9f5bcb6f156fc4ef17
Tine Marbjerg <tinemarbjerg@gmail.com>
2024-04-08 14:28:55 +0200

    commit H2 demo

--- src/test/java/demo/h2database/DepartmentRepositoryTest.java
+++ src/test/java/demo/h2database/DepartmentRepositoryTest.java
@@ -12,7 +12,7 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@ActiveProfiles("mysql")
+@ActiveProfiles("h2")
 class DepartmentRepositoryTest {
 
     @Autowired
